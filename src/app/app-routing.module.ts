3cd0c0f7c4fb20e3227b654c7dae7c960c4c1360
Sunil Kumar Singh <sunilsingh132000@gmail.com>
2025-04-27 11:51:53 +0530

    coding of product and product-detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { SpecificationDialogComponent } from './components/product-detail/specification-dialog/specification-dialog.component';
 
 const routes: Routes = [
   {path:"" , component:LandingComponent},
-  {path:"products" , component:ProductDetailComponent}
+  {path:"products" , component:ProductDetailComponent},
+  {path:"product-detail/:id" , component:SpecificationDialogComponent}
 ];
 
 @NgModule({
--- src/app/components/product-detail/specification-dialog/specification-dialog.component.ts
+++ src/app/components/product-detail/specification-dialog/specification-dialog.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../../services/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../../models/product.model';
 
 @Component({
   selector: 'app-specification-dialog',
   templateUrl: './specification-dialog.component.html',
-  styleUrl: './specification-dialog.component.css'
+  styleUrls: ['./specification-dialog.component.css']  // Make sure this is correct
 })
-export class SpecificationDialogComponent {
 
+export class SpecificationDialogComponent implements OnInit {
+
+  product:Product[]=[];
+   productId!: string | null; 
+  constructor(
+    private route: ActivatedRoute,  
+    private productService: ProductService  
+  ) {}
+
+  ngOnInit(): void {
+     this.productId = this.route.snapshot.paramMap.get('id');
+     console.log(this.productId);
+     this.getProductdetail()
+  }
+
+  getProductdetail(){
+    this.productService.getProduct().subscribe((data:Product[])=>{
+      console.log("spec",data);
+     this.product = data.filter(item=>{
+      return item.id === Number(this.productId); 
+     })
+    })
+  }
 }
